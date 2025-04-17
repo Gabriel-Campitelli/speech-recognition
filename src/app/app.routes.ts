86508e017699ec7e93d0d55973e27e24220f811a
Campitelli, Gabriel <gabriel.campitelli@mercer.com>
2025-04-17 11:30:36 -0300

    feat(init)[XXX-0]: routing issue fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './components/app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  { path: '**', redirectTo: 'value', pathMatch: 'full' },
+];
