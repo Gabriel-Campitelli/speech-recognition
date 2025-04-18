df96a3088e5fd747bbb29f371012deeceb2fbbd9
Campitelli, Gabriel <gabriel.campitelli@mercer.com>
2025-04-18 11:56:51 -0300

    feat(init)[XXX-0]: improve server.js

--- server.js
+++ server.js
@@ -1,34 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
 
-const allowCors = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, X-Access-Token, X-Key"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, POST, DELETE, OPTIONS, PATCH"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-};
+app.use(express.static(__dirname + "dist/speech-recognition-poc"));
 
-app.use(allowCors);
-app.use(bodyParser.json({ limit: "10mb" }));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/dist/speech-recognition-poc"));
-
-// SERVE ANGULAR index.html
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/dist/speech-recognition-poc/index.html");
+  res.sendFile(path.join(__dirname, "dist/speech-recognition-poc/index.html"));
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, async () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log(`API Gateway running in port ${port}!`);
 });
