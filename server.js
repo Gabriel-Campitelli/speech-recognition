d9e7c08e3a9a1066a96318143bfc5c69410f29de
Campitelli, Gabriel <gabriel.campitelli@mercer.com>
2025-04-18 11:58:23 -0300

    feat(init)[XXX-0]: improve server.js

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.get("*", (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`API Gateway running in port ${port}!`);
+  console.log(`API Gateway running in port ${PORT}!`);
 });
